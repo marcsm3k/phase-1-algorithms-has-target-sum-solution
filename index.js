e0a0d8aae5b2f7e28f2aa5148e80f27ffce72735
Marco <marcovelardejr@gmail.com>
2022-06-04 16:16:54 -0500

    commit tree 43218b718ce6347ec5df158d0f567c6375e2218e

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+  
+  function hasTargetSum(array, target) {
+    for (let i = 0; i < array.length; i++) {
+     
+      const complement = target - array[i];
+      for (let j = i + 1; j < array.length; j++) {
+        
+        if (array[j] === complement) return true;
+      }
+    }
+  
+    return false;
+  }
+  
+
 
 /* 
   Write the Big O time complexity of your function here
+
+  function hasTargetSum(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    // n steps (depending on the length of the input array)
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      // n * n steps (nested loop!)
+      if (array[j] === complement) return true;
+    }
+  }
+  // 1 step
+  return false;
+}
 */
 
 /* 
